ba2584ea90b8445336d0a8b11553710741ec9512
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2020-03-06 23:48:48 +0100

    :construction: Add separation on each image, working on script

---
+++ sandbox-01/index.js
@@ -0,0 +1,17 @@
+const $container = document.querySelector('.container')
+const $bar = $container.querySelector('.bar')
+const $img = $container.querySelectorAll('.img')
+
+$bar.addEventListener(
+    'click',
+    ()=>{
+        
+    }
+)
+
+const drag_and_drop_on = $bar.addEventListener(
+    'mouseover',
+    ()=>{
+        
+    }
+)
