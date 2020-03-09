1e1bae118e927b15c7cb9c816a4003255279f704
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2020-03-10 00:50:14 +0100

    :tada: Change img + Fix before after + Reusable code

--- sandbox-01/index.js
+++ sandbox-01/index.js
@@ -1,6 +1,7 @@
-const $container = document.querySelector('.container')
-const $bar = $container.querySelector('.bar')
-const $img = $container.querySelectorAll('.img')
+// Re usable part start here:
+const $lc_before_after_container = document.querySelector('.lc_before_after_container')
+const $lc_before_after_bar = $lc_before_after_container.querySelector('.lc_before_after_bar')
+const $lc_before_after_img = $lc_before_after_container.querySelectorAll('.lc_before_after_img')
 
 // Drag & Drop
 
@@ -26,17 +27,22 @@ const bar_mousemove = ()=>{
 
 // STEP 2.5 : Activate Movement of the bar & changing clip-path of img
 const bar_mousemove_function = (_event)=>{
-    const bounding = $container.getBoundingClientRect()
+    const bounding = $lc_before_after_container.getBoundingClientRect()
     const cursorX = _event.clientX
-    const cut_pos = (cursorX - bounding.x)-($bar.offsetWidth/2)
-    $bar.style.transform = `translateX(${cut_pos}px)`
-    $img[0].style.clipPath = `polygon(0 0, ${cut_pos}px 0, ${cut_pos}px 100%, 0% 100%)`
-    $img[1].style.clipPath = `polygon(100% 0, ${cut_pos}px 0, ${cut_pos}px 100%, 100% 100%)`
+    let cut_pos = (cursorX - bounding.x)-($lc_before_after_bar.offsetWidth/2)
+    // if (cursorX < bounding.x) {
+    //     cut_pos = 0 - $lc_before_after_bar.offsetWidth/2
+    // }
+    // else if(cursorX > (bounding.x + bounding.width)){
+    //     cut_pos = bounding.width - $lc_before_after_bar.offsetWidth/2
+    // }
+    $lc_before_after_bar.style.transform = `translateX(${cut_pos}px)`
+    $lc_before_after_img[0].style.clipPath = `polygon(0 0, ${cut_pos}px 0, ${cut_pos}px 100%, 0% 100%)`
+    $lc_before_after_img[1].style.clipPath = `polygon(100% 0, ${cut_pos}px 0, ${cut_pos}px 100%, 100% 100%)`
 }
 
 
 // STEP 1 : USER press down his mouse on $bar
-$bar.addEventListener('mousedown', bar_mousemove)
+$lc_before_after_bar.addEventListener('mousedown', bar_mousemove)
 
-
-
+// Re usable part end here
